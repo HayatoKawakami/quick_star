7267f0aa86e36aeb1afee50330a01a121a8a8a3a
HayatoKawakami <redbull.drip.mother@gmail.com>
2022-09-16 13:32:59 +0900

    railsをAPIモードに変更

--- src/config/application.rb
+++ src/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
@@ -20,3 +21,4 @@ module App
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
